24b84903161919a8de8f044c4079873634cadcb0
Mr.AlexanderSobko <mr.alexandersobko@yandex.ru>
2023-04-01 01:16:49 +0800

    fix: fixing code style

---
+++ src/main/java/ru/yandex/practicum/filmorate/config/TestConfig.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+@Configuration
+public class TestConfig {
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("schema.sql")
+                .addScripts("data.sql")
+                .build();
+    }
+}
